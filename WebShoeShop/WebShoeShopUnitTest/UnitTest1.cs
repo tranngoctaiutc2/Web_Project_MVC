6bd2c3e81fa2f3d1580fbe9f98df0244fe195b71
Huu Tai <tain6507@gmail.com>
2024-11-03 15:38:55 +0700

    Update Unit test

--- WebShoeShop/WebShoeShopUnitTest/UnitTest1.cs
+++ WebShoeShop/WebShoeShopUnitTest/UnitTest1.cs
@@ -1,4 +1,4 @@
-using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
@@ -46,15 +46,134 @@ namespace WebShoeShopUnitTest
         }
 
         [Test]
-        public void userFailed()
+        public void userBlank()
         {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
 
+            // Navigate to the login page
+            IWebElement profileBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".fa.fa-user")));
+            Assert.IsNotNull(profileBtn);
+            profileBtn.Click();
+
+            IWebElement loginBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            Assert.IsNotNull(loginBtn);
+            loginBtn.Click();
+
+            Assert.That(_driver.Url, Is.EqualTo("https://localhost:44390/account/login"));
+
+            IWebElement username = _driver.FindElement(By.Name("UserName"));
+            username.SendKeys(""); // Blank username
+
+            IWebElement password = _driver.FindElement(By.Name("Password"));
+            password.SendKeys("0985181215thanH@");
+
+            IWebElement loginSubmitBtn = _driver.FindElement(By.ClassName("form__button"));
+            loginSubmitBtn.Click();
+
+            IWebElement errorMessage = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".text-danger-username")));
+            Assert.IsNotNull(errorMessage);
+            Assert.That(errorMessage.Text, Is.EqualTo("Tên đăng nhập không được để trống")); // Adjusted error message
         }
 
         [Test]
-        public void passwordFailed()
+        public void PasswordBlank()
         {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
 
+            // Navigate to the login page
+            IWebElement profileBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".fa.fa-user")));
+            profileBtn.Click();
+
+            IWebElement loginBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            loginBtn.Click();
+
+            Assert.That(_driver.Url, Is.EqualTo("https://localhost:44390/account/login"));
+
+            // Leave password blank
+            IWebElement username = wait.Until(ExpectedConditions.ElementIsVisible(By.Name("UserName")));
+            username.SendKeys("admin"); // Valid username
+
+            IWebElement password = wait.Until(ExpectedConditions.ElementIsVisible(By.Name("Password")));
+            password.SendKeys(""); // Blank password
+
+            IWebElement loginSubmitBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("form__button")));
+            loginSubmitBtn.Click();
+
+            // Wait for the error message to be displayed
+            IWebElement errorMessage = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".text-danger-password")));
+            Assert.IsNotNull(errorMessage);
+            Assert.That(errorMessage.Text, Is.EqualTo("Mật khẩu không được để trống")); // Validate the message
         }
+
+
+        [Test]
+        public void loginFailedWithIncorrectUsername()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+
+            // Navigate to the login page
+            IWebElement profileBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".fa.fa-user")));
+            Assert.IsNotNull(profileBtn);
+            profileBtn.Click();
+
+            IWebElement loginBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            Assert.IsNotNull(loginBtn);
+            loginBtn.Click();
+
+            Assert.That(_driver.Url, Is.EqualTo("https://localhost:44390/account/login"));
+
+            IWebElement username = _driver.FindElement(By.Name("UserName"));
+            username.SendKeys("invalidUser"); // Invalid username
+
+            IWebElement password = _driver.FindElement(By.Name("Password"));
+            password.SendKeys("0985181215thanH@"); // Invalid password
+
+            IWebElement loginSubmitBtn = _driver.FindElement(By.ClassName("form__button"));
+            loginSubmitBtn.Click();
+
+            IWebElement errorMessage = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".error-message")));
+            Assert.IsNotNull(errorMessage);
+            Assert.That(errorMessage.Text, Is.EqualTo("Đăng nhập không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.")); // Adjusted error message
+        }
+
+        [Test]
+        public void loginFailedWithIncorrectPassword()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+
+            // Navigate to the login page
+            IWebElement profileBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".fa.fa-user")));
+            Assert.IsNotNull(profileBtn);
+            profileBtn.Click();
+
+            IWebElement loginBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            Assert.IsNotNull(loginBtn);
+            loginBtn.Click();
+
+            Assert.That(_driver.Url, Is.EqualTo("https://localhost:44390/account/login"));
+
+            IWebElement username = _driver.FindElement(By.Name("UserName"));
+            username.SendKeys("admin"); // Invalid username
+
+            IWebElement password = _driver.FindElement(By.Name("Password"));
+            password.SendKeys("invalidPassword"); // Invalid password
+
+            IWebElement loginSubmitBtn = _driver.FindElement(By.ClassName("form__button"));
+            loginSubmitBtn.Click();
+
+            IWebElement errorMessage = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".error-message")));
+            Assert.IsNotNull(errorMessage);
+            Assert.That(errorMessage.Text, Is.EqualTo("Đăng nhập không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.")); // Adjusted error message
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (_driver != null)
+            {
+                _driver.Quit();
+            }
+        }
+
     }
 }
