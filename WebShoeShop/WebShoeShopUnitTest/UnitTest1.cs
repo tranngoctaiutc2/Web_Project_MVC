91ca4e8eb994760f02679f04c07f2ec11a65a5f4
tranngoctaiutc2 <tranngoct330@gmail.com>
2024-11-25 09:56:46 +0700

    Add Test

--- WebShoeShop/WebShoeShopUnitTest/UnitTest1.cs
+++ WebShoeShop/WebShoeShopUnitTest/UnitTest1.cs
@@ -105,6 +105,26 @@ namespace WebShoeShopUnitTest
             Assert.That(errorMessage.Text, Is.EqualTo("Mật khẩu không được để trống")); // Validate the message
         }
 
+        [Test]
+        public void LoginWithoutCredentials()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+
+            IWebElement profileBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".fa.fa-user")));
+            profileBtn.Click();
+
+            IWebElement loginBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            loginBtn.Click();
+
+            Assert.That(_driver.Url, Is.EqualTo("https://localhost:44390/account/login"));
+
+            IWebElement loginSubmitBtn = _driver.FindElement(By.ClassName("form__button"));
+            loginSubmitBtn.Click();
+
+            IWebElement errorMessage = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".text-danger-username")));
+            Assert.IsNotNull(errorMessage);
+            Assert.That(errorMessage.Text, Is.EqualTo("Tên đăng nhập không được để trống"));
+        }
 
         [Test]
         public void loginFailedWithIncorrectUsername()
@@ -164,6 +184,59 @@ namespace WebShoeShopUnitTest
             IWebElement errorMessage = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".error-message")));
             Assert.IsNotNull(errorMessage);
             Assert.That(errorMessage.Text, Is.EqualTo("Đăng nhập không thành công. Vui lòng kiểm tra lại thông tin đăng nhập.")); // Adjusted error message
+        }
+        [Test]
+        public void LoginAndRedirectToDashboard()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+
+            IWebElement profileBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".fa.fa-user")));
+            profileBtn.Click();
+
+            IWebElement loginBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            loginBtn.Click();
+
+            Assert.That(_driver.Url, Is.EqualTo("https://localhost:44390/account/login"));
+
+            IWebElement username = _driver.FindElement(By.Name("UserName"));
+            username.SendKeys("admin");
+
+            IWebElement password = _driver.FindElement(By.Name("Password"));
+            password.SendKeys("0985181215thanH@");
+
+            IWebElement loginSubmitBtn = _driver.FindElement(By.ClassName("form__button"));
+            loginSubmitBtn.Click();
+
+            // Kiểm tra URL chuyển hướng
+            wait.Until(ExpectedConditions.UrlToBe("https://localhost:44390/"));
+            Assert.That(_driver.Url, Is.EqualTo("https://localhost:44390/"));
+        }
+
+        [Test]
+        public void LoginWithSQLInjection()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
+
+            IWebElement profileBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector(".fa.fa-user")));
+            profileBtn.Click();
+
+            IWebElement loginBtn = wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("a[href='/account/login']")));
+            loginBtn.Click();
+
+            Assert.That(_driver.Url, Is.EqualTo("https://localhost:44390/account/login"));
+
+            IWebElement username = _driver.FindElement(By.Name("UserName"));
+            username.SendKeys("' OR '1'='1");
+
+            IWebElement password = _driver.FindElement(By.Name("Password"));
+            password.SendKeys("' OR '1'='1");
+
+            IWebElement loginSubmitBtn = _driver.FindElement(By.ClassName("form__button"));
+            loginSubmitBtn.Click();
+
+            IWebElement errorMessage = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(".error-message")));
+            Assert.IsNotNull(errorMessage);
+            Assert.That(errorMessage.Text, Is.EqualTo("Đăng nhập không thành công. Vui lòng kiểm tra lại thông tin đăng nhập."));
         }
 
         [TearDown]
